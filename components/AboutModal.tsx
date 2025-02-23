f88b7426c963b0748df6edbfd976df7322278941
Ron Avital <ronavital2@gmail.com>
2025-02-23 13:57:55 +0200

    added selected poits and about page

--- components/AboutModal.tsx
+++ components/AboutModal.tsx
@@ -8,22 +8,20 @@ const ScrollableAlert = ({ visible, onClose }: AboutModalProps) => {
       <View style={styles.modalContainer}>
         <View style={styles.alertContainer}>
           <ScrollView contentContainerStyle={styles.scrollContainer}>
-            <Text style={styles.title}>About Run App</Text>
-            <Text style={styles.bodyText}>Welcome to **Run App** – your ultimate companion for planning and discovering the best running routes! Whether you're a casual jogger or a seasoned marathoner, we provide tools to find, create, and customize routes that fit your specific preferences.</Text>
+            <Text style={styles.title}>About Running Route Generator</Text>
+            <Text style={styles.bodyText}>Welcome to Running Route Generator – your ultimate companion for planning and discovering the best running routes! Whether you're a casual jogger or a seasoned marathoner, we provide tools to find, create, and customize routes that fit your specific preferences.</Text>
             <Text style={styles.bodyText}>Our mission is to make running more enjoyable and accessible for everyone. We believe that running is not just about fitness – it's about exploring the world, pushing your limits, and experiencing the joy of movement. Our app is designed to help you discover new routes, stay motivated, and track your progress, all while fostering a supportive running community.</Text>
             <Text style={styles.bodyText}>
               Features include:
               {"\n"}- Route Generator: Find routes tailored to your preferred starting and ending points, route length, and difficulty level.
-              {"\n"}- Route Suggestions: Browse through a curated list of suggested running routes based on your preferences and location.
-              {"\n"}- Map Themes: Customize the appearance of your map with multiple themes, including Dark, Aubergine, Retro, and more.
+              {"\n"}- AI assisted: Our AI chat-bot helps you plan your running route based on your preferences, ensuring a personalized running experience.
             </Text>
             <Text style={styles.bodyText}>
               Why Run App?
               {"\n"}- Easy to Use: Our user-friendly interface makes it easy to search, customize, and save routes.
               {"\n"}- Smart Features: Whether you prefer short jogs, long-distance runs, or specific terrain, our app adapts to your needs.
-              {"\n"}- Community-Focused: We’re not just about running; we’re about building a community of runners who share their experiences, challenges, and victories.
             </Text>
-            <Text style={styles.bodyText}>Get in touch with us for any questions or feedback at: support@runapp.com</Text>
+            <Text style={styles.bodyText}>Get in touch with us for any questions or feedback at: **********************support@runapp.com</Text>
           </ScrollView>
           <TouchableOpacity style={styles.closeButton} onPress={onClose}>
             <Text style={styles.closeText}>Close</Text>
